cc7d15a63d16260671fb059b4ea3576b1941824f
Vijay Nathani <vijay_nathani@yahoo.com>
2021-07-26 22:35:12 +0530

    Unneeded Java examples deleted

--- JavaExamples/CreateOne.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LINE="============================================================"
-while read A; do
-	echo $LINE
-	echo "$A"
-	echo $LINE
-	cat $A
-	echo " "
-done < <(find -name 'Q*.java' | sort)  > "CombinedFile.txt"
